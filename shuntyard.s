e33daee9b356b10817e61d7c5b61e298b594a8f8
Jérôme Tanghe <tanghe.jerome1@gmail.com>
2015-03-16 16:59:27 +0100

    shuntyard

--- shuntyard.s
+++ shuntyard.s
@@ -1,12 +1,53 @@
+.data
+	nbre: .long 3, '+', 4
+.bss
+	queue: .space 20
+
+.text
 # shuntyard
 # Converts the operation to infix notation
 # Arguments:
 #	- The string to convert
+
+.globl _start
+_start:
+	MOV nbre, %eax
+	PUSH %eax
+	CALL shuntyard
+
 .globl shuntyard
 .type  shuntyard function
 shuntyard:
 	POP return
 	POP %eax
-	MOV $0, %ebx
+	MOV queue, %ecx
 
-	MOV (%eax), %ebx
+loop:
+	MOV (%eax), %bl
+
+	CMPB %bl, '\0'
+		JE endofloop
+
+	CMPB %bl, '+'
+		JE operator
+	CMPB %bl, '-'
+		JE operator
+	CMPB %bl, '*'
+		JE operator
+	CMPB %bl, '/'
+		JE operator
+	
+	MOVB %bl, (%ecx)
+	INC %ecx
+
+	INC %eax
+	JMP loop
+	
+operator:
+	PUSH %ebx
+	INC %eax
+	JMP loop
+
+endofloop:
+	POP %ebx
+	MOVB %bl, (%ecx)
